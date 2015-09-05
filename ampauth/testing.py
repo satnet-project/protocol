8bc13ccc26512338d5b6db9b0f82d694417e0101
Samuel Gongora Garcia <s.gongoragarcia@gmail.com>
2015-09-05 20:56:54 +0200

    (backup)

--- ampauth/testing.py
+++ ampauth/testing.py
@@ -28,6 +28,7 @@ from twisted.python import failure, log
 from twisted.cred import portal, checkers, error, credentials
 from twisted.internet import defer
 from twisted.python import log
+from twisted.spread import pb
 
 from django.test import Client, override_settings
 from django.conf import settings
@@ -74,9 +75,50 @@ class DjangoAuthChecker():
         except User.DoesNotExist:
             raise error.UnauthorizedLogin("Incorrect username.")
 
-class Testing():
 
-    def create_user_profile(self, username, password, email):
-        print username
-        print password
-        print email
+"""
+Reads the elements from the database and create a list of twisted creds.
+"""
+class DjangoAuthCheckers():
+    implements(checkers.ICredentialsChecker)
+    credentialInterfaces = (credentials.IUsernamePassword,
+    credentials.IUsernameHashedPassword)
+
+    def _passwordMatch(self, matched, user):
+        if matched:
+            return user
+        else:
+            return failure.Failure(error.UnauthorizedLogin())
+
+    def requestAvatarId(self, credentials):
+        try:
+            user = User.objects.get(username=credentials.username)
+            return defer.maybeDeferred(
+                check_password,
+                credentials.password,
+                user.password).addCallback(self._passwordMatch, user)
+        except User.DoesNotExist:
+            return defer.fail(error.UnauthorizedLogin())
+
+
+class Realm(object):
+    implements(portal.IRealm)
+
+    def requestAvatar(self, user, mind, *interfaces):
+        assert pb.IPerspective in interfaces
+        avatar = Avatar(user)
+        avatar.attached(mind)
+        return pb.IPerspective, avatar, lambda a=avatar:a.detached(mind)
+
+
+class Avatar(pb.Avatar):
+    def __init__(self, user):
+        self.user = user
+
+    def attached(self, mind):
+        self.remote = mind
+        print 'User %s connected' % (self.user,)
+
+    def detached(self, mind):
+        self.remote = None
+        print 'User %s disconnected' % (self.user,)
