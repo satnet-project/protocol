0b059395657081b113b663aa03bd86c65804b95c
Samuel Gongora Garcia <s.gongoragarcia@gmail.com>
2015-10-23 13:09:22 +0200

    Dependencies updated.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,12 @@ script_path="$( cd "$( dirname "$0" )" && pwd )"
 project_path=$( readlink -e "$script_path/.." )
 venv_path="$project_path/.venv"
 
+# Install required packages
+sudo apt --assume-yes install build-essential
+sudo apt --assume-yes install python-dev
+sudo apt --assume-yes install python-pip
+sudo apt --assume-yes install virtualenv
+
 # Create a virtualenv
 virtualenv $venv_path
 source "$venv_path/bin/activate"
