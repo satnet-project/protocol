aa27886e869bb00f97d542e6e22446310d7669af
sgongar <s.gongoragarcia@gmail.com>
2015-12-09 14:04:51 +0100

    (update)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 script_path="$( cd "$( dirname "$0" )" && pwd )"
 project_path=$( readlink -e "$script_path/.." )
-venv_path="$project_path/.venv"
 
-# Install required packages
-sudo apt --assume-yes install build-essential
-sudo apt --assume-yes install python-dev
-sudo apt --assume-yes install python-pip
-sudo apt --assume-yes install virtualenv
-sudo apt --assume-yes install libffi-dev
-sudo apt --assume-yes install libssl-dev
-sudo apt --assume-yes install libpq-dev
+if [ $1 == '-travisCI'];
+then
+	venv_path="$project_path/.venv"
 
-# Create a virtualenv
-virtualenv $venv_path
-source "$venv_path/bin/activate"
-pip install -r "$project_path/requirements.txt"
+	# Install required packages
+	sudo apt --assume-yes install build-essential
+	sudo apt --assume-yes install python-dev
+	sudo apt --assume-yes install python-pip
+	sudo apt --assume-yes install virtualenv
+	sudo apt --assume-yes install libffi-dev
+	sudo apt --assume-yes install libssl-dev
+	sudo apt --assume-yes install libpq-dev
 
-# Create a self-signed certicate
-bash "$script_path/protocol-setup.sh"
+	# Create a virtualenv
+	virtualenv $venv_path
+	source "$venv_path/bin/activate"
+	pip install -r "$project_path/requirements.txt"
+
+	# Create a self-signed certicate
+	bash "$script_path/protocol-setup.sh"
+fi
