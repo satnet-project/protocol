7ccb205d22ca8df7a07aba9fdad6e629c8479d37
sgongar <s.gongoragarcia@gmail.com>
2015-12-10 12:38:07 +0100

    (update)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -59,4 +59,25 @@ then
 	# Create a self-signed certicate
 	# bash "$script_path/protocol-setup.sh"
 
+elif [ $1 == '-circleCI' ];
+then
+	venv_path="$project_path/.venv"
+
+	# Install required packages
+	# sudo apt --assume-yes install build-essential
+	# sudo apt --assume-yes install python-dev
+	# sudo apt --assume-yes install python-pip
+	# sudo apt --assume-yes install virtualenv
+	# sudo apt --assume-yes install libffi-dev
+	# sudo apt --assume-yes install libssl-dev
+	# sudo apt --assume-yes install libpq-dev
+
+	# Create a virtualenv
+	# virtualenv $venv_path
+	# source "$venv_path/bin/activate"
+	pip install -r "$script_path/requirements-tests.txt"
+
+	# Create a self-signed certicate
+	# bash "$script_path/protocol-setup.sh"
+
 fi
