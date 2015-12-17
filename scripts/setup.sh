ba785541bc9bc45ac3cfff8129cadeb255da37bc
sgongar <s.gongoragarcia@gmail.com>
2015-12-17 19:00:31 +0100

    Update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+#    Copyright 2015 Samuel Góngora García
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# :Author:
+#     Samuel Góngora García (s.gongoragarcia@gmail.com)
+
+# __author__ = 's.gongoragarcia@gmail.com'
+
 script_path="$( cd "$( dirname "$0" )" && pwd )"
 project_path=$( readlink -e "$script_path/.." )
 
@@ -161,6 +181,7 @@ then
 	rm ~/Desktop/satnetprotocol.desktop
 	rm ~/Escritorio/satnetprotocol.desktop
 
+	echo ">>> Removing dependencies"
 	sudo apt --assume-yes remove supervisor
 	sudo apt --assume-yes remove build-essential
 	sudo apt --assume-yes remove python-dev
