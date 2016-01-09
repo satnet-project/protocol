2dcb82c0c6dda6908cd8f5bb5209e9289504d12e
Ricardo Tubío-Pardavila <rtubiopa@calpoly.edu>
2016-01-09 07:55:50 -0800

    .gitignore skips now temporary text files, added init.d startup example script, setup.sh script simplified

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #    Copyright 2015 Samuel Góngora García
-
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-
+#
 # :Author:
 #     Samuel Góngora García (s.gongoragarcia@gmail.com)
-
+#
 # __author__ = 's.gongoragarcia@gmail.com'
 
 function create_selfsigned_keys()
@@ -39,13 +39,15 @@ function create_selfsigned_keys()
     openssl genrsa -des3 -passout pass:satnet -out $keys_private 1024
     # 2: Generate a CSR (Certificate Signing Request)
     echo '>>> Generating a CSR'
-    openssl req -new -key $keys_private -passin pass:satnet -out $keys_csr -subj "/CN=$keys_CN"
+    openssl req -new -key $keys_private -passin pass:satnet\
+    	-out $keys_csr -subj "/CN=$keys_CN"
     # 3: Remove Passphrase from Private Key
     echo '>>> Removing passphrase from private key'
     openssl rsa -in $keys_private -passin pass:satnet -out $keys_private
     # 4: Generating a Self-Signed Certificate
     echo '>>> Generating a public key (certificate)'
-    openssl x509 -req -days 365 -in $keys_csr -signkey $keys_private -out $keys_crt
+    openssl x509 -req -days 365 -in $keys_csr -signkey $keys_private\
+    	-out $keys_crt
 
     echo '>>> Generating key bundles'
     # 5: Generate server bundle (Certificate + Private key)
@@ -75,6 +77,22 @@ function create_supervisor_conf()
 
 }
 
+function remove_supervisor_conf()
+{
+
+	echo ">>> Removing satnet-protocol's configuration from within supervisord"
+
+	[[ -f $output_conf_file ]] && {
+
+		sudo rm -f $output_conf_file
+		echo ">>> $output_conf_file removed"
+
+	} || {
+		echo ">>> <$output_conf_file> not found, skipping removal..."
+	}
+
+}
+
 function create_runner_sh()
 {
 
@@ -82,7 +100,7 @@ function create_runner_sh()
     echo "" | sudo tee -a $runner_sh
     echo "source $venv_dir/bin/activate" | sudo tee -a $runner_sh
     # echo "python server_amp.py" | sudo tee -a $runner_sh
-    echo "twistd procmon python server_amp.py" | sudo tee -a $runner_sh    
+    echo "twistd procmon python server_amp.py" | sudo tee -a $runner_sh
     echo "" | sudo tee -a $runner_sh
 
     chmod +x $runner_sh
@@ -94,6 +112,12 @@ function install_packages()
 	echo ">>> Installing system packages..."
 	sudo aptitude update && sudo aptitude dist-upgrade -y
 	sudo aptitude install $( cat "$linux_packages" ) -y
+}
+
+function uninstall_packages()
+{
+	echo ">>> Uninstalling system packages..."
+	sudo aptitude remove $( cat "$linux_packages" ) -y
 }
 
 function install_venv()
@@ -163,18 +187,20 @@ _install_desk_shortcuts='false'
 if [ $1 == '-install' ];
 then
 
+	echo ">>> Installing satnet-protocol..."
+
     [[ $_install_packages == 'true' ]] && install_packages
     [[ $_install_venv == 'true' ]] && install_venv
 
     # This installation steps are not mandatory (GUI not required)
 
-	[[ $_generate_keys == 'true' ]] && create_selfsigned_keys
+    [[ $_generate_keys == 'true' ]] && create_selfsigned_keys
     [[ $_create_logs == 'true' ]] && create_logs_dir
 
     [[ $_config_supervisor == 'true' ]] && configure_supervisor
 
     [[ $_reboot == 'true' ]] && {
- 
+
     	echo ">>> To apply changes you must reboot your system"
     	echo ">>> Reboot now? (yes/no)"
     	read OPTION
@@ -182,219 +208,39 @@ then
 
     }
 
+	exit 0
+
 fi
 
 if [ $1 == '-travisCI' ];
 then
 
-	# Install required packages
-	# sudo apt --assume-yes install build-essential
-	# sudo apt --assume-yes install python-dev
-	# sudo apt --assume-yes install python-pip
-	# sudo apt --assume-yes install virtualenv
-	# sudo apt --assume-yes install libffi-dev
-	# sudo apt --assume-yes install libssl-dev
-	# sudo apt --assume-yes install libpq-dev
+	echo ">>> [TravisCI] Installing satnet-protocol..."
+    [[ $_install_venv == 'true' ]] && install_venv
+	exit 0
 
-	# Create a virtualenv
-	# virtualenv $venv_dir
-	# source "$venv_dir/bin/activate"
-	pip install -r "$script_path/requirements-tests.txt"
-
-	# Create a self-signed certicate
-	# bash "$script_path/protocol-setup.sh"
 fi
 
 if [ $1 == '-circleCI' ];
 then
 
-	# Install required packages
-	# sudo apt --assume-yes install build-essential
-	# sudo apt --assume-yes install python-dev
-	# sudo apt --assume-yes install python-pip
-	# sudo apt --assume-yes install virtualenv
-	# sudo apt --assume-yes install libffi-dev
-	# sudo apt --assume-yes install libssl-dev
-	# sudo apt --assume-yes install libpq-dev
+	echo ">>> [CircleCI] Installing satnet-protocol..."
+    [[ $_install_venv == 'true' ]] && install_venv
+    exit 0
 
-	# Create a virtualenv
-	# virtualenv $venv_dir
-	# source "$venv_dir/bin/activate"
-	pip install -r "$script_path/requirements-tests.txt"
-
-	# Create a self-signed certicate
-	# bash "$script_path/protocol-setup.sh"
 fi
 
 if [ $1 == '-uninstall' ];
 then
-	echo ">>> Removing program files"
-	sudo rm -r -f /opt/satnet/
 
-	echo ">>> Removing the daemon for SATNet protocol"
-	sudo rm /usr/local/bin/satnet-protocol.sh
-	sudo rm /etc/supervisor/conf.d/satnet-protocol-conf.conf
+	echo ">>> Removing satnet-protocol..."
+	echo ">>> NOTICE: this command only removes external dependencies"
+	echo ">>> NOTICE: to fully remove this program, delete this directory"
 
-	echo ">>> Removing old logs"
-	rm ~/.satnet/logs/*
+	[[ $_config_supervisor == 'true' ]] && remove_supervisor_conf
+	[[ $_install_packages == 'true' ]] && uninstall_packages
 
-	echo ">>> Removing executables"
-	rm ~/bin/satnetprotocol
-
-	echo ">>> Removing links"
-	rm ~/Desktop/satnetprotocol.desktop
-	rm ~/Escritorio/satnetprotocol.desktop
-
-	echo ">>> Removing dependencies"
-	sudo apt --assume-yes remove supervisor
-	sudo apt --assume-yes remove build-essential
-	sudo apt --assume-yes remove python-dev
-	sudo apt --assume-yes remove python-pip
-	sudo apt --assume-yes remove virtualenv
-	sudo apt --assume-yes remove libffi-dev
-	sudo apt --assume-yes remove libssl-dev
-	sudo apt --assume-yes remove libpq-dev
-	sudo apt --assume-yes remove shc
-	sudo apt --assume-yes remove unzip
-
-	echo ">>> Do you wish to remove all configuration files? (yes/no)"
-	read OPTION
-	if [ $OPTION == 'yes' ];
-	then
-		rm -r -f ~/.satnet
-	fi
-fi
-
-
-if [ $1 == '-update' ];
-then
-	echo ">>> Removing program files"
-	sudo rm -r -f /opt/satnet/
-
-	echo ">>> Removing the daemon for SATNet protocol"
-	sudo rm /usr/local/bin/satnet-protocol.sh
-	sudo rm /etc/supervisor/conf.d/satnet-protocol-conf.conf
-
-	echo ">>> Removing old logs"
-	rm ~/.satnet/logs/*
-
-	echo ">>> Removing executables"
-	rm ~/bin/satnetprotocol
-
-	echo ">>> Removing links"
-	rm ~/Desktop/satnetprotocol.desktop
-	rm ~/Escritorio/satnetprotocol.desktop
-
-	echo ">>> Removing dependencies"
-	sudo apt --assume-yes remove supervisor
-	sudo apt --assume-yes remove build-essential
-	sudo apt --assume-yes remove python-dev
-	sudo apt --assume-yes remove python-pip
-	sudo apt --assume-yes remove virtualenv
-	sudo apt --assume-yes remove libffi-dev
-	sudo apt --assume-yes remove libssl-dev
-	sudo apt --assume-yes remove libpq-dev
-	sudo apt --assume-yes remove shc
-	sudo apt --assume-yes remove unzip
-
-	echo ">>> Do you wish to remove all configuration files? (yes/no)"
-	read OPTION
-	if [ $OPTION == 'yes' ];
-	then
-		rm -r -f ~/.satnet
-	fi
-
-	venv_dir="$project_path/.venv"
-
-	# Install required packages
-	sudo apt --assume-yes install build-essential
-	sudo apt --assume-yes install python-dev
-	sudo apt --assume-yes install python-pip
-	sudo apt --assume-yes install virtualenv
-	sudo apt --assume-yes install libffi-dev
-	sudo apt --assume-yes install libssl-dev
-	sudo apt --assume-yes install libpq-dev
-	sudo apt --assume-yes install shc
-	sudo apt --assume-yes --force-yes install unzip
-
-	# Create a virtualenv
-	virtualenv $venv_dir
-	source "$venv_dir/bin/activate"
-	pip install -r "$project_path/requirements.txt"
-
-	# Downloading packages for GUI
-	# Needed to install SIP first
-	cd $venv_dir
-	mkdir build && cd build
-	pip install SIP --allow-unverified SIP --download="."
-	unzip sip*
-	echo "antes de uno"
-	pwd
-	ls
-	cd sip*
-	python configure.py
-	make
-	sudo make install
-	cd ../ && rm -r -f sip*
-
-	# PyQt4 installation.
-	wget http://downloads.sourceforge.net/project/pyqt/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
-	tar xvzf PyQt-x11-gpl-4.11.4.tar.gz
-	cd PyQt-x11-gpl-4.11.4
-	python ./configure.py --confirm-license --no-designer-plugin -q /usr/bin/qmake-qt4 -e QtGui -e QtCore
-	make
-	# Bug. Needed ldconfig, copy it from /usr/sbin
-	cp /sbin/ldconfig ../../bin/
-	sudo ldconfig
-	sudo make install
-	cd ../ && rm -r -f PyQt*
-
-	# Create a self-signed certicate
-	bash "$script_path/protocol-setup.sh"
-
-	# Supervisor install
-	sudo apt --assume-yes install supervisor
-
-	echo ">>> This script will generate a daemon for SATNet protocol"
-	echo ">>> Populating directories"
-	mkdir ~/.satnet/
-	mkdir ~/.satnet/logs/
-	sudo mkdir /opt/satnet
-	sudo cp -r -f ../../protocol /opt/satnet/
-
-	currentUser=$(whoami)
-
-	cd $script_path
-	sudo shc -f satnetprotocol.sh
-	sudo rm satnetprotocol.sh.x.c
-	sudo chmod 777 satnetprotocol.sh.x
-	sudo chown $currentUser satnetprotocol.sh.x
-	mv satnetprotocol.sh.x satnetprotocol
-
-	mkdir ~/bin/
-	mv satnetprotocol ~/bin/
-
-	cp satnetprotocol.desktop ~/Escritorio
-	cp satnetprotocol.desktop ~/Desktop
-
-	echo ">>> Copying daemons"
-	sudo cp satnet-protocol.sh /usr/local/bin
-	sudo chmod +x /usr/local/bin/satnet-protocol.sh
-	sudo cp satnet-protocol-conf.conf /etc/supervisor/conf.d/
-
-	echo ">>> Updating Supervisor"
-	sudo supervisorctl reread
-	sudo supervisorctl update
-
-	currentUser=$(whoami)
-	sudo chown $currentUser ~/.satnet/logs/* 
-
-	echo ">>> For apply changes you must reboot your system"
-	echo ">>> Reboot now? (yes/no)"
-	read OPTION
-	if [ $OPTION == 'yes' ];
-	then
-		sudo reboot
-	fi
+	exit 0
 
 fi
+
