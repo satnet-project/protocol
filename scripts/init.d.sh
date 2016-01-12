8ece53c29da3787e97af831eb30f7612f032ac9b
sgongar <s.gongoragarcia@gmail.com>
2016-01-12 15:16:29 +0100

    Script improved.

--- scripts/init.d.sh
+++ scripts/init.d.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 ### BEGIN INIT INFO
-# Provides:          ghserver
+# Provides:          satnetprotocol
 # Required-Start:    $local_fs $remote_fs $network $syslog
 # Required-Stop:     $local_fs $remote_fs $network $syslog
 # Default-Start:     2 3 4 5
@@ -8,26 +8,26 @@
 # Short-Description: Start/stop GHServer server
 ### END INIT INFO
 
-logger "GHServer: Start script executed"
+logger "satnetprotocol: Start script executed"
 # Test values
-GH_SERVER_PATH="/home/sgongar/Dev/protocol"
+SATNET_PROTOCOL_PATH="/home/sgongar/Dev/protocol"
 export PYTHONPATH="$GH_SERVER_PATH:$PYTHONPATH"
 
 case "$1" in
   start)
-    logger "GHServer: Starting"
-    echo "Starting GHServer..."
-    # twistd -y "$GH_SERVER_PATH/ghserverapp.py" -l "$GH_SERVER_PATH/ghserver.log" --pidfile "$GH_SERVER_PATH/twistd.pid"
-    twistd -y "$GH_SERVER_PATH/server_amp.py" -l "$GH_SERVER_PATH/server_amplog.log" --pidfile "GH_SERVER_PATH/twistd.pid"
+    logger "satnetprotocol: Starting"
+    echo "Starting SatNet protocol..."
+    source "$SATNET_PROTOCOL_PATH/.venv/bin/activate"
+    twistd -y "$SATNET_PROTOCOL_PATH/server_amp.py" -l "$SATNET_PROTOCOL_PATH/server_amplog.log" --pidfile "SATNET_PROTOCOL_PATH/twistd.pid"
     ;;
   stop)
-    logger "GHServer: Stopping"
-    echo "Stopping GHServer..."
-    kill `cat $GH_SERVER_PATH/twistd.pid`
+    logger "satnetprotocol: Stopping"
+    echo "Stopping SatNet protocol..."
+    kill `cat $SATNET_PROTOCOL_PATH/twistd.pid`
     ;;
   *)
-    logger "GHServer: Invalid usage"
-    echo "Usage: /etc/init.d/ghserver {start|stop}"
+    logger "satnetprotocol: Invalid usage"
+    echo "Usage: /etc/init.d/satnetprotocol {start|stop}"
     exit 1
     ;;
 esac
