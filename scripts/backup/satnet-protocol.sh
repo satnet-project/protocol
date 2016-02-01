f9da53015a16df63edaff53c043e6889f45ed026
sgongar <s.gongoragarcia@gmail.com>
2016-02-01 18:26:36 +0100

    Removed unnecesary files.

--- scripts/backup/satnet-protocol.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd "/opt/satnet/protocol/"
-source ".venv/bin/activate"
-python server_amp.py
-
--- scripts/backup/satnetprotocol.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /opt/satnet/protocol/
-source ".venv/bin/activate"
-python log_viewer.py
