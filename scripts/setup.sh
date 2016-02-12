6cf9c94f65485e3825ebc13f54fed14723196c25
Ricardo Tubío-Pardavila <rtubiopa@calpoly.edu>
2016-02-12 14:18:43 -0800

    Minor fixes: merge left HEADER at setup.sh and the arrow package was missing within requirements.txt

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-script_path="$( cd "$( dirname "$0" )" && pwd )"
-project_path=$( readlink -e "$script_path/.." )
-venv_path="$project_path/.venv"
-
-# Install required packages
-sudo apt --assume-yes install build-essential 
-sudo apt --assume-yes install virtualenv
-
-# Create a virtualenv
-virtualenv $venv_path
-source "$venv_path/bin/activate"
-pip install -r "$project_path/requirements.txt"
-=======
 
 #    Copyright 2015 Samuel Góngora García
 #
